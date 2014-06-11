f3212d1126d2fecb66fed1a2efed62ed7bf1b7cf
Jim Kubicek <jkubicek@twitter.com>
2014-06-10 17:31:22 -0700

    Added the swift app delegate

---
+++ Stocks/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Stocks
+//
+//  Created by Jim Kubicek on 6/10/14.
+//  Copyright (c) 2014 schwa.io. All rights reserved.
+//
+
+import UIKit
+
+class AppDelegate2: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
+extension AppDelegate2 { // UIApplicationDelegate
+
+    func application(application: UIApplication!,
+        didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+            return true
+    }
+
+}
